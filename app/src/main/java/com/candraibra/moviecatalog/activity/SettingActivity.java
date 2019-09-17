f1ac1e86e1beaa341a7943393a141335f87cc443
Candraibra <canisanie@gmail.com>
2019-09-17 12:26:09 +0700

    notif update

--- app/src/main/java/com/candraibra/moviecatalog/activity/SettingActivity.java
+++ app/src/main/java/com/candraibra/moviecatalog/activity/SettingActivity.java
@@ -1,25 +1,112 @@
 package com.candraibra.moviecatalog.activity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SwitchCompat;
 
 import com.candraibra.moviecatalog.R;
+import com.candraibra.moviecatalog.notif.DailyReceiver;
+import com.candraibra.moviecatalog.notif.ReleaseReceiver;
+import com.candraibra.moviecatalog.notif.preference;
 
 
 public class SettingActivity extends AppCompatActivity implements View.OnClickListener {
+    DailyReceiver dailyReceiver;
+    ReleaseReceiver realeaseReciver;
+    preference notificationPreference;
+    SharedPreferences spReleaseReminder, spDailyReminder;
+    SharedPreferences.Editor edtReleaseReminder, edtDailyReminder;
+
+    String TYPE_DAILY = "reminderDaily";
+    String TYPE_RELEASE = "reminderRelease";
+    String DAILY_REMINDER = "dailyReminder";
+    String RELEASE_REMINDER = "releaseReminder";
+    String KEY_RELEASE = "Release";
+    String KEY_DAILY_REMINDER = "Daily";
+
+    String timeDaily = "07:00";
+    String timeRelease = "08:00";
+    private SwitchCompat swDaily;
+    private SwitchCompat swRealise;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+        dailyReceiver = new DailyReceiver();
+        realeaseReciver = new ReleaseReceiver();
+        notificationPreference = new preference(this);
+        setPreference();
+
         TextView back = findViewById(R.id.backButton);
         back.setOnClickListener(this);
         TextView language = findViewById(R.id.tv_language);
         language.setOnClickListener(this);
+        swDaily = findViewById(R.id.sw_daily);
+        swRealise = findViewById(R.id.sw_realease);
+    }
+
+    private void releaseOn() {
+        String message = getResources().getString(R.string.realise_reminder_notif);
+        notificationPreference.setTimeRelease(timeRelease);
+        notificationPreference.setReleaseMessage(message);
+        realeaseReciver.setAlarm(SettingActivity.this, TYPE_RELEASE, timeRelease, message);
+    }
+
+    private void releaseOff() {
+        realeaseReciver.cancelNotif(SettingActivity.this);
+    }
+
+    private void dailyOn() {
+        String message = getResources().getString(R.string.daily_reminder);
+        notificationPreference.setTimeDaily(timeDaily);
+        notificationPreference.setDailyMessage(message);
+        dailyReceiver.setAlarm(SettingActivity.this, TYPE_DAILY, timeDaily, message);
+    }
+
+    private void dailyOff() {
+        dailyReceiver.CancelNotif(SettingActivity.this);
+    }
+
+    private void setPreference() {
+        spDailyReminder = getSharedPreferences(DAILY_REMINDER, MODE_PRIVATE);
+        boolean checkDailyReminder = spDailyReminder.getBoolean(KEY_DAILY_REMINDER, false);
+        swDaily.setChecked(checkDailyReminder);
+        spReleaseReminder = getSharedPreferences(RELEASE_REMINDER, MODE_PRIVATE);
+        boolean checkUpcomingReminder = spReleaseReminder.getBoolean(KEY_RELEASE, false);
+        swRealise.setChecked(checkUpcomingReminder);
+    }
+
+    public void setDaily(boolean isChecked) {
+        edtDailyReminder = spDailyReminder.edit();
+        if (isChecked) {
+            edtDailyReminder.putBoolean(KEY_DAILY_REMINDER, true);
+            edtDailyReminder.apply();
+            dailyOn();
+        } else {
+            edtDailyReminder.putBoolean(KEY_DAILY_REMINDER, false);
+            edtDailyReminder.commit();
+            dailyOff();
+        }
+    }
+
+    public void setRelease(boolean isChecked) {
+        edtReleaseReminder = spReleaseReminder.edit();
+        if (isChecked) {
+            edtReleaseReminder.putBoolean(KEY_RELEASE, true);
+            edtReleaseReminder.apply();
+            releaseOn();
+        } else {
+            edtReleaseReminder.putBoolean(KEY_RELEASE, false);
+            edtReleaseReminder.commit();
+            releaseOff();
+        }
     }
 
     @Override
