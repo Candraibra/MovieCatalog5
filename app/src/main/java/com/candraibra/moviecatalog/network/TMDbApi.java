acbe05a08cb98e34abaca11f37be76048600da7a
Candraibra <canisanie@gmail.com>
2019-09-18 11:22:44 +0700

    api search update

--- app/src/main/java/com/candraibra/moviecatalog/network/TMDbApi.java
+++ app/src/main/java/com/candraibra/moviecatalog/network/TMDbApi.java
@@ -12,7 +12,7 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface TMDbApi {
-
+    //popular_movie
     @GET("movie/popular")
     Call<MoviesResponse> getPopularMovies(
             @Query("api_key") String apiKey,
@@ -20,6 +20,7 @@ public interface TMDbApi {
             @Query("page") int page
     );
 
+    //upcoming_movie
     @GET("movie/upcoming")
     Call<MoviesResponse> getUpcomingMovies(
             @Query("api_key") String apiKey,
@@ -27,6 +28,21 @@ public interface TMDbApi {
             @Query("page") int page
     );
 
+    //search_movie
+    @GET("search/movie")
+    Call<MoviesResponse> getSearchMovie(
+            @Query("query") String query,
+            @Query("api_key") String apiKey
+    );
+
+    //search_tv
+    @GET("search/tv")
+    Call<TvResponse> getSearchTv(
+            @Query("query") String query,
+            @Query("api_key") String apiKey
+    );
+
+    //popular_tv
     @GET("tv/popular")
     Call<TvResponse> getPopularTv(
             @Query("api_key") String apiKey,
@@ -34,6 +50,7 @@ public interface TMDbApi {
             @Query("page") int page
     );
 
+    //on_the_air_tv
     @GET("tv/on_the_air")
     Call<TvResponse> getUpcomingTv(
             @Query("api_key") String apiKey,
@@ -41,12 +58,14 @@ public interface TMDbApi {
             @Query("page") int page
     );
 
+    //genre_movies
     @GET("genre/movie/list")
     Call<GenreResponse> getGenres(
             @Query("api_key") String apiKey,
             @Query("language") String language
     );
 
+    //get_movie_by_id
     @GET("movie/{movie_id}")
     Call<Movie> getMovie(
             @Path("movie_id") int id,
@@ -54,6 +73,7 @@ public interface TMDbApi {
             @Query("language") String language
     );
 
+    //get_tv_by_id
     @GET("tv/{tv_id}")
     Call<Tv> getTv(
             @Path("tv_id") int id,
