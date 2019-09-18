4da6885d17ce994c45e390726b50a4d51906d03e
Candraibra <canisanie@gmail.com>
2019-09-18 11:46:36 +0700

    layout update

--- app/src/main/java/com/candraibra/moviecatalog/activity/SearchTvActivity.java
+++ app/src/main/java/com/candraibra/moviecatalog/activity/SearchTvActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import com.candraibra.moviecatalog.R;
 
-public class SearchMovieActivity extends AppCompatActivity {
-    public static String SEARCH_MOVIE = "query";
+public class SearchTvActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search_movie);
+        setContentView(R.layout.activity_search_tv);
     }
 }
